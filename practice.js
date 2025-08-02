45ffb3ee789bd3f908a4d0ea23dc06bf50a32a58
anshul-kdev <someshkushwaha9517@gmail.com>
2025-08-02 16:49:15 +0000

    fully functional now working weather app

--- practice.js
+++ practice.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const apiKey = process.env.API_KEY;
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
+const searchBtn = document.getElementById("searchBtn");
+const city = document.getElementById("city");
+
+async function checkweather(city){
+    const weather = await fetch(apiUrl + city + `&appid=${apiKey}&units=metric`);
+    const data = await weather.json();
+    if(weather.status == 404){
+        document.getElementById("cityname").innerHTML = "City not found";
+    } else {
+            console.log(data);
+            document.getElementById("cityname").innerHTML = city;
+            document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+            document.getElementById("visibility").innerHTML = data.visibility + "%";
+        document.getElementById("windspeed").innerHTML = data.visibility + "%";
+        }
+}
+searchBtn.addEventListener("click",() => {
+     checkweather(city.value);
+    
+})
